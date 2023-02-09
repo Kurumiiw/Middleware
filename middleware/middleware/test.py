675f49f4eb3faca1c8e8a2528088e6977200514a
Ola Horg Jacobsen <olahja@stud.ntnu.no>
2023-02-09 14:45:45 +0100

    #5: Simple socket bind on custom address

--- middleware/middleware/test.py
+++ middleware/middleware/test.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+import socket
+
+HOST = input("addr: ")
+MIDDLEWARE_PORT = 5000
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print("[Socket] Binding address", HOST, "with port",
+          MIDDLEWARE_PORT, "for", s.type, "...")
+    s.bind((HOST, MIDDLEWARE_PORT))
+
+    print("[Socket] Listening")
+    s.listen()
