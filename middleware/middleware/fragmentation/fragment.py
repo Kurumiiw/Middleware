780a1b6fca7f55642f3b058921087cd6aaafd23e
Thorbjørn Lundin <51087536+thorbjoernl@users.noreply.github.com>
2023-02-16 13:50:08 +0100

    9: Various fixes from comments and refactoring.
    - Byte order is set appropriately.
    - Too large packets raises exception during fragmentation.
    - Unfragmented packet is identified by packetid of 0. Fragmented packets do
    not contain original header.
    - Fixed misleading comment.

---
+++ middleware/middleware/fragmentation/fragment.py
@@ -0,0 +1,152 @@
+from __future__ import annotations
+from middleware.fragmentation.packet import Packet
+from typing import Iterator, List, Union
+
+
+MAX_TCP_HEADER_BYTES = 60
+
+
+class MissingFragmentException(ValueError):
+    """
+    Raised when a fragment is missing during attempted reassembly.
+    """
+
+    pass
+
+
+class EffectiveMTUTooLowException(ValueError):
+    """
+    Raised when adjusting provided MTU for TCP/header overhead results in a value lower than or equal to 0.
+    """
+
+    pass
+
+
+class PacketTooLarge(ValueError):
+    """
+    Raised when a packet being fragmented is too large to be fragmented.
+    """
+
+    pass
+
+
+class Fragment(Packet):
+    # Static variable for keeping track of global package id counter.
+    # TODO: Should probably keep a separate counter per service to reduce chance
+    # of collision, or do it in a better way.
+    packet_id_counter: int = 0
+
+    def __init__(self, data: bytearray, /, packet_id: int, seq: int, final: int):
+        if packet_id == 0:
+            raise ValueError("Packet ID of 0 is reserved for unfragmented packets.")
+        if len(data) == 0:
+            raise ValueError(
+                "Unexpected data length. Data must be greater than 0 in length."
+            )
+
+        if seq > final:
+            raise ValueError("Fragment ID is greater than final fragment ID.")
+
+        self.data = bytearray()
+        self.data.extend(packet_id.to_bytes(4, byteorder="little"))
+        self.data.extend(seq.to_bytes(2, byteorder="little"))
+        self.data.extend(final.to_bytes(2, byteorder="little"))
+
+        self.data.extend(data)
+
+    def get_header(self):
+        return self.data[0:8]
+
+    def get_data(self):
+        return self.data[8:]
+
+    def get_fragment_number(self) -> int:
+        """
+        Returns the fragment id portion of the header.
+        """
+        return int.from_bytes(self.get_header()[4:6], byteorder="little", signed=False)
+
+    def get_last_fragment_number(self) -> int:
+        """
+        Returns the final fragment id in this sequence.
+        """
+        return int.from_bytes(self.get_header()[6:8], byteorder="little", signed=False)
+
+    @staticmethod
+    def fragment(
+        packet: Packet, /, mtu: int = 500
+    ) -> Iterator[Union[Fragment, Packet]]:
+        """
+        Fragments the packet appropriately for the chosen MTU.
+        """
+        # TODO: Finding a better way to determine TCP header size would avoid some overhead.
+        # Perhaps we can assume that our Middleware makes no packets with additional TCP header options?
+        effective_mtu = mtu - MAX_TCP_HEADER_BYTES - 8
+
+        if effective_mtu <= 0:
+            raise EffectiveMTUTooLowException(
+                "Adjusting mtu for TCP and header overhead resulted in a negative MTU. Please choose a larger MTU."
+            )
+
+        # No need to fragment if size is already < effective_mtu
+        if packet.get_size() <= effective_mtu:
+            return iter(packet)
+
+        offset = 0
+        counter = 0
+        final = int(packet.get_data_size() / effective_mtu)
+
+        print(final)
+        if final > 65535:  # 2byte unsigned integer max.
+            raise PacketTooLarge("Packet is too large to be fragmented properly.")
+
+        pid = Fragment.get_next_packet_id()
+        while offset < packet.get_data_size():
+            yield Fragment(
+                packet.get_data()[offset : (offset + effective_mtu)],
+                packet_id=pid,
+                seq=counter,
+                final=final,
+            )
+
+            offset = offset + effective_mtu
+            counter = counter + 1
+
+        return
+
+    @staticmethod
+    def reorder(fragments: List[Fragment]) -> List[Fragment]:
+        """
+        Reorders a list of fragments according to their sequence numbers.
+        """
+        return sorted(list(fragments), key=lambda p: p.get_fragment_number())
+
+    @staticmethod
+    def reassemble(fragments: Iterator[Fragment]) -> Packet:
+        """
+        Reassembles a collection of fragments into the original packet. Fragments should
+        contain only fragments from the original packet, but can be unordered.
+        """
+        # Check for missing packets.
+        if len(fragments) != fragments[0].get_last_fragment_number():
+            raise MissingFragmentException(
+                "A packet was missing during attempt at reassembly."
+            )
+
+        result = bytearray()
+        result.extend(bytearray([0, 0, 0, 0]))
+
+        for x in Fragment.reorder(list(fragments)):
+            result.extend(x.get_data())
+
+        return Packet(result)
+
+    @staticmethod
+    def get_next_packet_id() -> int:
+        """
+        Increments the global packet id counter and returns the new value.
+        """
+        Fragment.packet_id_counter = Fragment.packet_id_counter + 1
+        if Fragment.packet_id_counter > 4294967295:  # 4 byte unsigned int max.
+            Fragment.packet_id_counter = 1
+        return Fragment.packet_id_counter
--- middleware/tests/fragmentation/test_packet.py
+++ middleware/tests/fragmentation/test_packet.py
@@ -1,30 +1,19 @@
 import pytest
 import random
-from middleware.fragmentation.packet import (
-    Packet,
-    MissingFragmentException,
-    EffectiveMTUTooLowException,
-)
+from middleware.fragmentation.packet import Packet
 
-SAMPLE_DATA = [145, 234, 255, 245]
+
+SAMPLE_DATA = bytearray([145, 234, 255, 245])
 
 
 def test_packet_creation():
     """
     Tests combinations of packet creation options.
     """
-    Packet.packet_id_counter = 0
-    p = Packet(bytearray([1, 2, 3, 4]))
+    p = Packet(SAMPLE_DATA)
 
-    assert p.data == bytearray([1, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4])
-
-    p = Packet(bytearray([1, 2, 3, 4]), create_header=False)
-
-    assert p.data == bytearray([1, 2, 3, 4])
-
-    p = Packet(bytearray([1, 2, 3, 4]), seq=5, final=7)
-
-    assert p.data == bytearray([2, 0, 0, 0, 5, 0, 7, 0, 1, 2, 3, 4])
+    assert p.get_data() == SAMPLE_DATA
+    assert p.get_header() == bytearray([0, 0, 0, 0])
 
 
 def test_create_empty_packet_exception():
@@ -33,76 +22,3 @@ def test_create_empty_packet_exception():
     """
     with pytest.raises(ValueError) as e:
         Packet(bytearray(0))
-
-
-def test_fragmentation():
-    """
-    Tests whether fragmentation and reassembly of ordered fragments works.
-    """
-    Packet.packet_id_counter = 0
-
-    p = Packet(bytearray(SAMPLE_DATA))
-
-    assert p.data == bytearray([1, 0, 0, 0, 0, 0, 0, 0]) + bytearray(SAMPLE_DATA)
-
-    # MTU chosen to be 69, so the adjusted mtu = 1, i.e 1 byte of data per packet.
-    fragments = tuple(Packet.fragment(p, mtu=69))
-
-    # Check packet id being equal in fragments.
-    for x in fragments[1:]:
-        assert x.get_packet_id() == fragments[0].get_packet_id()
-
-    assert len(fragments) == 12
-
-    p2 = Packet.reassemble(fragments)
-
-    assert p.data == p2.data
-
-
-def test_unordered_packet_reassembly():
-    """
-    Tests whether assembling an out-of-order list of fragments works.
-    """
-    p = Packet(bytearray(SAMPLE_DATA))
-
-    # Fragment packet to fragments with one byte of data each.
-    fragments = list(Packet.fragment(p, mtu=69))
-
-    random.shuffle(fragments)
-
-    p2 = Packet.reassemble(fragments)
-
-    assert p.data == p2.data
-
-
-def test_missing_fragment_exception():
-    """
-    Tests whether reassembling packets with missing packets results in an Exception being raised.
-    """
-    # Test with a packet in the middle missing.
-    p = Packet(bytearray(SAMPLE_DATA))
-
-    fragments = list(Packet.fragment(p, mtu=69))
-
-    fragments.remove(fragments[3])
-
-    with pytest.raises(MissingFragmentException):
-        Packet.reassemble(fragments)
-
-    # Test with an end packet missing.
-    p2 = Packet(bytearray(SAMPLE_DATA))
-
-    fragments = list(Packet.fragment(p2, mtu=69))[:2]
-
-    with pytest.raises(MissingFragmentException):
-        Packet.reassemble(fragments)
-
-
-def test_small_adjusted_mtu_exception():
-    """
-    Test that setting MTU too low, (i.e. fragmentation is impossible due to overhead), results in an exception being raised.
-    """
-    p = Packet(bytearray(SAMPLE_DATA))
-
-    with pytest.raises(EffectiveMTUTooLowException):
-        fragments = list(Packet.fragment(p, mtu=0))
