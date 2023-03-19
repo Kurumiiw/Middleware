4af031cb7ce462ffb81251b7f48ab9dcef9f7ebe
Thorbjørn Lundin <51087536+thorbjoernl@users.noreply.github.com>
2023-03-19 13:10:59 +0100

    Fix

--- middleware/test_from_doc.py
+++ middleware/test_from_doc.py
@@ -1,21 +1,27 @@
 from middleware.middlewareAPI import *
 import threading
 
+# client_1 = MiddlewareAPI.reliable("", 5000, TOS=0x8, MTU=150, timeout=10)
+# client_2 = MiddlewareAPI.reliable("", 5005, TOS=0x8, MTU=150, timeout=10)
 
-client_1 = MiddlewareAPI.reliable("", 5000, TOS=0x8, MTU=150, timeout=10)
-client_2 = MiddlewareAPI.reliable("", 5005, TOS=0x8, MTU=150, timeout=10)
+# client_2.bind(("", 5005))
 
-client_2.bind(("", 5005))
 
-def receive():
-    client_receive, addr = client_2.accept()
-    received_data = client_receive.receive()
-    print(received_data)
+def waitForPacket(mwSocket):
+    conn, addr = mwSocket.accept()
+    assert conn.receive() == b"Hello there"
+    conn.send(b"General Kenobi")
 
-thr = threading.Thread(target=receive, daemon=True)
-thr.start()
-client_1.connect(("localhost", 5005))
-client_1.send(b"Hello there")
 
-received_data = client_2.receive()
-print(received_data)
+mwReceive = MiddlewareAPI.reliable("", 5001)
+mwSend = MiddlewareAPI.reliable("", 5006)
+# Workaround for address already in use error causing failed tests when running pytest
+# multiple times in quick succession.
+mwReceive.socko.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+mwReceive.bind(("", 5001))
+mwReceive.listen()
+threading.Thread(target=waitForPacket, args=(mwReceive,)).start()
+mwSend.connect(("localhost", 5001))
+mwSend.send(b"Hello there")
+received = mwSend.receive()
+print(received)
