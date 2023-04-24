341fad3eadb2233ad3391224c77b9d8b6fc33f8d
Ola Horg Jacobsen <olahja@stud.ntnu.no>
2023-04-24 19:33:20 +0200

    27 TCP tuning using test service (#49)
    * #27 Began some work on test_service and an config
    
    Co-authored-by: Ola Vanni Flaata <olavfla@users.noreply.github.com>
    
    * Made configurable workers that send data to controller in different threads
    
    * Fixed path for config file
    
    * #27 Test service that imitates traffic from Emil's service
    
    * #27: test_service seperated into worker and controller.
    
    Co-authored-by: Ola Vanni Flaata <ola.flaata@gmail.com>
    
    * #27: test_service debug information
    
    Co-Authored-By: Ola Vanni Flaata <ola.flaata@gmail.com>
    
    * #27: TCP tuning
    
    Co-Authored-By: Ola Vanni Flaata <ola.flaata@gmail.com>
    
    * #27: NATO narrowband config
    
    * #27: B L A C K
    
    ---------
    
    Co-authored-by: Tobias Thrane <Tobias@thrane.name>
    Co-authored-by: Ola Vanni Flaata <olavfla@users.noreply.github.com>
    Co-authored-by: Ola Vanni Flaata <ola.flaata@gmail.com>

---
+++ middleware/services/test_service/controller.py
@@ -0,0 +1,95 @@
+import sys
+import os
+from middleware.middlewareAPI import *
+import threading
+import json
+import random
+import time
+import signal
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+
+class Controller:
+    """
+    Should:
+    - Load test configuration from config file and configure worker(s).
+        - Simple port discovery when connecting to workers.
+        - Configuration should support setting a distribution of packet size to send, what type of connection to use (reliable/unreliable) and whether traffic should be spaced evenly or in bursts.
+    - Run tests and collect statistics.
+    - Reporting of test results
+        - RTT, packet lost, retransmission overhead, amount of data received/sent on either end, etc.
+        - Console
+        - HTML report (??)
+    """
+
+    def __init__(self, ip, port):
+        self.connections = {}
+        self.running = True
+        self.unreliable = MiddlewareUnreliable()
+        self.reliable = MiddlewareReliable()
+        self.reliable.bind((ip, port))
+        self.reliable.listen(5)
+        self.unreliable.bind((ip, port))
+
+    def print_statistics(self, addr=None):
+        if addr == None:
+            for i in self.connections:
+                stats = self.connections[i]
+                total_data_len = 0
+                for j in stats:
+                    total_data_len += len(j[0])
+                print(i, ":")
+                print("Total data", total_data_len)
+                print(
+                    "Average bandwidth:",
+                    (total_data_len / (stats[-1][1] - stats[0][1])) * 8,
+                )
+        else:
+            stats = self.connections[addr]
+            total_data_len = 0
+            for j in stats:
+                total_data_len += len(j[0])
+            print(addr, ":")
+            print("Total data:", total_data_len)
+            print(
+                "Average bandwidth:",
+                (total_data_len / (stats[-1][1] - stats[0][1])) * 8,
+            )
+
+    def run(self):
+        def run_reliable():
+            def run_conn(conn, addr):
+                self.connections[addr] = []
+                while True:
+                    data = conn.recv(2048)
+                    self.connections[addr].append((data, time.time()))
+                    if data == b"":
+                        self.print_statistics(addr)
+                        break
+
+            while True:
+                print("Ready to connect")
+                conn, addr = self.reliable.accept()
+                threading.Thread(
+                    target=run_conn, args=(conn, addr), daemon=True
+                ).start()
+                print(f"New connection from {addr}")
+
+        def run_unreliable():
+            data, address = self.unreliable.recvfrom()
+            if address in self.connections.keys():
+                self.connections[address].append((data, time.time()))
+            else:
+                self.connections[address] = [(data, time.time())]
+
+        rel_thread = threading.Thread(target=run_reliable, daemon=True)
+        unrel_thread = threading.Thread(target=run_unreliable, daemon=True)
+        rel_thread.start()
+        unrel_thread.start()
+
+
+if __name__ == "__main__":
+    controller = Controller("", 5001)
+    controller.run()
+    input()
---
+++ middleware/services/test_service/test_service.py
@@ -0,0 +1,264 @@
+import sys
+import os
+from middleware.middlewareAPI import *
+import threading
+import json
+import random
+import time
+
+""" 
+Configurable test service for sending a lot of random data according to some configuration 
+(eg. lots of small packets/a few big packets / reliable/unreliable / burst/regular interval. 
+For received data, send back a hash of the data to source to allow for verification. 
+Collect statistics about the provided service, and report it.
+Should allow for systematic testing of many network patterns, and realistic sending patterns.
+"""
+
+
+class Controller:
+    """
+    Should:
+    - Load test configuration from config file and configure worker(s).
+        - Simple port discovery when connecting to workers.
+        - Configuration should support setting a distribution of packet size to send, what type of connection to use (reliable/unreliable) and whether traffic should be spaced evenly or in bursts.
+    - Run tests and collect statistics.
+    - Reporting of test results
+        - RTT, packet lost, retransmission overhead, amount of data received/sent on either end, etc.
+        - Console
+        - HTML report (??)
+    """
+
+    def __init__(self):
+        self.config = None
+        self.workers = {}
+        self.connections = {}
+        self.running = False
+        self.unreliable = MiddlewareUnreliable()
+        self.reliable = MiddlewareReliable()
+
+    def configure(self, filepath):
+        with open(filepath, "r") as f:
+            self.config = json.load(f)
+
+        self.reliable.bind(("", 5000))
+        self.reliable._socko.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.reliable.listen(len(self.config["workers"]))
+
+        # Configure workers
+        for worker in self.config["workers"]:
+            self.workers[worker["name"]] = Worker(worker)
+            if worker["reliable"]:
+                conn, _ = self.reliable.accept()
+                conn.settimeout(0)
+                self.connections[worker["name"]] = conn
+
+        print(len(self.workers))
+        self.unreliable.bind(("", 5000))
+        self.unreliable.settimeout(0)
+
+    def run_simultaneous(self):
+        self.running = True
+        for worker in self.workers.values():
+            worker.start()
+        LINE_BACK = "\033[1A"
+        LINE_CLEAR = "\033[2K"
+        while any([worker.is_alive() for worker in self.workers.values()]):
+            for conn in self.connections.values():
+                try:
+                    conn.recv(256 * 256)
+                except:
+                    pass
+            time.sleep(0.05)
+            for worker in self.workers.values():
+                print(
+                    f"Progress {worker.name}: {(worker.total_packets-worker.progress)/worker.total_packets*100:.2f}%"
+                )
+            print(LINE_BACK * len(self.workers), end=LINE_CLEAR)
+        self.running = False
+
+    def run_single(self, name):
+        self.workers[name].start()
+
+        while self.workers[name].is_alive():
+            if self.workers[name].reliable:
+                try:
+                    data = self.connections[name].recv(256 * 256)
+                    self.workers[name].statistics["receive_bytes"] += len(data)
+                    print(
+                        f"Progress {name}: {(self.workers[name].total_packets-self.workers[name].progress)/self.workers[name].total_packets*100:.2f}%",
+                        end="\r",
+                    )
+                except Exception as e:
+                    pass
+            else:
+                try:
+                    data, _ = self.unreliable.recvfrom()
+                    self.workers[name].statistics["receive_bytes"] += len(data)
+                    print(
+                        f"Progress {name}: {(self.workers[name].total_packets-self.workers[name].progress)/self.workers[name].total_packets*100:.2f}%",
+                        end="\r",
+                    )
+                except Exception as e:
+                    pass
+            time.sleep(0.05)
+        self.running = False
+        print()
+        print(f"Received: {self.workers[name].statistics['receive_bytes']}")
+        print(f"Sent: {self.workers[name].statistics['send_bytes']}")
+        print(f"Data bytest sent: {self.workers[name].statistics['data_bytes']}")
+        print(
+            f"Overhead bytes: {self.workers[name].statistics['send_bytes']-self.workers[name].statistics['data_bytes']}"
+        )
+        print(
+            f"Overhead ratio: {(self.workers[name].statistics['send_bytes']-self.workers[name].statistics['data_bytes'])/self.workers[name].statistics['send_bytes']*100:.2f}%"
+        )
+
+
+class Worker(threading.Thread):
+    """
+    Should:
+    - Receive and set configuration from Controller.
+    - Echo data as well as measured statistics to Controller.
+    """
+
+    def __init__(self, config):
+        # Config legend:
+        # mtu: Maximum transmission unit
+        # name: Name of worker. i.e. "file_transfer" should be named based on analogy to real-world service.
+        # TOS: Type of service value
+        # reliable: Whether to use reliable or unreliable connection
+        # port: Port to listen on
+
+        # packet_size: Size of packets to send
+        # packet_size_variance: Variance of packet size. 0.1 means 10% variance
+
+        # send_interval: Interval between sending packets. 0 means send as fast as possible.
+        # send_interval_variance: Variance of send interval. 0.1 means 10% variance. Does not apply if send_interval is 0.
+
+        # burst_size: Number of packets to send in a burst. 0 means send packets at regular intervals.
+        # burst_size_variance: Variance of burst size. 0.1 means 10% variance. Does not apply if burst_size is 0.
+
+        # burst_interval: Interval between bursts. 0 means send packets as fast as possible.
+        # burst_interval_variance: Variance of burst interval. 0.1 means 10% variance. Does not apply if burst_interval is 0.
+
+        # total_packets: Total number of packets to send
+
+        # The difference between send_interval and burst_interval is that send_interval is the interval between sending packets, while burst_interval is the interval between bursts (or groups of packets). If burst_size is 0, then burst_interval is ignored.
+
+        threading.Thread.__init__(self, daemon=True)
+        self.mtu = config["mtu"]
+        self.name = config["name"]
+        self.TOS = config["TOS"]
+        self.reliable = config["reliable"]
+        self.port = config["port"]
+
+        self.packet_size = config["packet_size"]
+        self.packet_size_variance = config["packet_size_variance"]
+
+        self.send_interval = config["send_interval"]
+        self.send_interval_variance = config["send_interval_variance"]
+
+        self.burst_size = config["burst_size"]
+        self.burst_size_variance = config["burst_size_variance"]
+        self.burst_interval = config["burst_interval"]
+        self.burst_interval_variance = config["burst_interval_variance"]
+
+        self.total_packets = config["total_packets"]
+        self.progress = self.total_packets
+
+        if self.reliable:
+            self.sock = MiddlewareReliable()
+            self.sock._socko.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+            if self.port != 0:
+                self.sock.bind(("", self.port))
+                print(f"Worker {self.name} bound to port {self.port} (reliable)")
+            self.sock.connect(("localhost", 5000))
+        else:
+            self.sock = MiddlewareUnreliable()
+            if self.port != 0:
+                print(f"Worker {self.name} bound to port {self.port} (unreliable)")
+                self.sock.bind(("", self.port))
+            else:
+                print(f"Worker {self.name} bound wildcard port (unreliable)")
+        self.sock.set_tos(self.TOS)
+
+        self.statistics = {
+            "name": self.name,
+            "send_events": [],
+            "receive_events": [],
+            "data_bytes": 0,
+            "send_bytes": 0,
+            "receive_bytes": 0,
+        }
+
+    def run(self):
+        self.running = True
+
+        def send_packet(packet):
+            if self.reliable:
+                self.statistics["send_bytes"] += self.sock.send(packet)
+            else:
+                self.statistics["send_bytes"] += self.sock.sendto(
+                    packet, ("localhost", 5000)
+                )
+            self.statistics["send_events"].append(
+                {"time": time.time(), "size": len(packet)}
+            )
+
+        # connect to controller
+
+        while self.running:
+            packet_size = round(
+                self.packet_size
+                * (
+                    1
+                    + random.uniform(
+                        -self.packet_size_variance, self.packet_size_variance
+                    )
+                )
+            )
+            send_interval = self.send_interval * (
+                1
+                + random.uniform(
+                    -self.send_interval_variance, self.send_interval_variance
+                )
+            )
+            burst_size = min(
+                round(
+                    self.burst_size
+                    * (
+                        1
+                        + random.uniform(
+                            -self.burst_size_variance, self.burst_size_variance
+                        )
+                    )
+                ),
+                self.progress,
+            )
+            burst_interval = self.burst_interval * (
+                1
+                + random.uniform(
+                    -self.burst_interval_variance, self.burst_interval_variance
+                )
+            )
+
+            while burst_size > 0:
+                packet = random.randbytes(packet_size)
+                self.statistics["data_bytes"] += packet_size
+                send_packet(packet)
+                burst_size -= 1
+                self.progress -= 1
+                time.sleep(send_interval / 1000)
+            time.sleep(burst_interval / 1000)
+            if self.progress <= 0:
+                self.running = False
+                self.done = True
+
+
+if __name__ == "__main__":
+    controller = Controller()
+    path = os.path.dirname(os.path.realpath(__file__))
+    controller.configure(path + "/config.json")
+    # controller.run_simultaneous()
+    input("Press enter to start test...")
+    controller.run_single("emil_issue")
